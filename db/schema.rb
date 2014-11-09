98172f6c19d03550a759b7f45609f98c4d01bf2e
Ivan Zarea <zarea.ion@gmail.com>
2014-11-09 08:41:16 +0200

    Generated the pg_search migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108113246) do
+ActiveRecord::Schema.define(version: 20141109064011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pg_search_documents", force: true do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "questions", force: true do |t|
     t.string "number",              null: false
