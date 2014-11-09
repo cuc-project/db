4508f44da5bf75851ce54e5db408bdb2a826709b
Ivan Zarea <zarea.ion@gmail.com>
2014-11-09 09:23:41 +0200

    Added pg_searchable to the Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,8 @@
 class Question < ActiveRecord::Base
+  include PgSearch
+  pg_search_scope :search_in_questions,
+                  against: :text,
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
