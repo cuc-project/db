95fce2552c63e62d3c5643ddc9ab504d67b8ec9c
Ivan Zarea <zarea.ion@gmail.com>
2014-11-09 09:37:36 +0200

    Removed the unnecessary .keep file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109064011) do
+ActiveRecord::Schema.define(version: 20141109073343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141109064011) do
     t.text   "comment"
     t.text   "source"
     t.string "authors_names"
+    t.string "team_name"
   end
 
 end
