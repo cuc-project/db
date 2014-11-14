17e8b4af217b28febd478d7e0ce299871815081a
Ivan Zarea <zarea.ion@gmail.com>
2014-11-14 16:13:10 +0200

    Counting the questions properly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 class SearchController < ApplicationController
   layout "search"
   def index
+    @count = Question.count
   end
 
   def search
     @terms   = sanitize_terms(params["query"])
     @results = Question.search_by_text(@terms).page(params[:page])
+    @count   = Question.count
     render :results
   end
 
