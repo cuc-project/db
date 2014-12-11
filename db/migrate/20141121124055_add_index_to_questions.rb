71a42c5c86d9e45b4fa81adb2bd710cfdeb812ce
Ivan Zarea <zarea.ion@gmail.com>
2014-12-11 18:18:23 +0200

    Removed unnecessary stuffs

--- db/migrate/20141121124055_add_index_to_questions.rb
+++ db/migrate/20141121124055_add_index_to_questions.rb
@@ -13,13 +13,3 @@ class AddIndexToQuestions < ActiveRecord::Migration
       }
   end
 end
-# def change
-#   # => Create gin index for materialized view
-#   execute %q{
-#     drop index if exists admin_clients_search_idx;
-#     create index admin_clients_search_idx ON clients
-#     using gin( (to_tsvector('simple', coalesce("clients"."id"::text, '')) ||
-#     to_tsvector('simple', coalesce("clients"."name"::text, '')) ||
-#     to_tsvector('simple', coalesce("clients"."email"::text, '')) ) );
-#   }
-# end
