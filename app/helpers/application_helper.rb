7b22a796803826e833a733993d62aa13a6237718
Ivan Zarea <zarea.ion@gmail.com>
2015-02-26 21:28:28 +0200

    oops.O

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def nav_link_to(title, path, options = {})
-    options[:class] += ' active' if current_page?(path)
+    if current_page?(path) and options[:class]
+      options[:class] += ' active'
+    end
     link_to(title, path, options)
   end
 end
